8baf4099a46b20a6ae6453ab470df243699900dd
xuexiangjys <xuexiangjys@163.com>
2021-01-10 17:57:12 +0800

    升级xpage至最新版本

--- app/src/main/java/com/xuexiang/templateproject/utils/sdkinit/XBasicLibInit.java
+++ app/src/main/java/com/xuexiang/templateproject/utils/sdkinit/XBasicLibInit.java
@@ -23,7 +23,6 @@ import com.xuexiang.templateproject.MyApp;
 import com.xuexiang.templateproject.core.BaseActivity;
 import com.xuexiang.templateproject.utils.XToastUtils;
 import com.xuexiang.xaop.XAOP;
-import com.xuexiang.xpage.AppPageConfig;
 import com.xuexiang.xpage.PageConfig;
 import com.xuexiang.xrouter.launcher.XRouter;
 import com.xuexiang.xui.XUI;
@@ -75,13 +74,7 @@ public final class XBasicLibInit {
      */
     private static void initXPage(Application application) {
         PageConfig.getInstance()
-                //页面注册
-                .setPageConfiguration(context -> {
-                    //自动注册页面,是编译时自动生成的，build一下就出来了
-                    return AppPageConfig.getInstance().getPages();
-                })
                 .debug(MyApp.isDebug() ? "PageLog" : null)
-                .enableWatcher(MyApp.isDebug())
                 .setContainActivityClazz(BaseActivity.class)
                 .init(application);
     }
